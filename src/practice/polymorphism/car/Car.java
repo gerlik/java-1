55817d4ec9e00194144d55da9154d59e9a433512
gerlik <gerliks@gmail.com>
2018-10-31 16:11:39 +0200

    Start olymorphism with cars

---
+++ src/practice/polymorphism/car/Car.java
@@ -0,0 +1,31 @@
+package practice.polymorphism.car;
+
+public class Car {
+    private String engine;
+    private int cylinders;
+    private String carType;
+
+    public void startEngine() {
+
+    }
+
+    public void accelerate() {
+
+    }
+
+    public void brake() {
+
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+}
---
+++ src/practice/polymorphism/car/Main.java
@@ -0,0 +1,7 @@
+package practice.polymorphism.car;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

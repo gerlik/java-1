9c0203a8d9f9e21b559d390e9699a1a2a8089d3f
gerlik <gerliks@gmail.com>
2018-11-07 14:18:56 +0200

    New task

---
+++ src/ylesanne2/Tere.java
@@ -0,0 +1,11 @@
+package ylesanne2;
+
+// Output: "tere" or whatever text bigger with symbols *
+public class Tere {
+    public static void main(String[] args) {
+
+        // User inserts text
+
+        //Output
+    }
+}

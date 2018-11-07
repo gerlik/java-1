1a4ef6520dc309b69372709bff7135090b237d5e
gerlik <gerliks@gmail.com>
2018-11-07 14:17:51 +0200

    Started new tasks

--- src/practice/arrays/Main.java
+++
@@ -1,5 +0,0 @@
-package practice.arrays;
-
-public class Main {
-
-}
---
+++ src/practice/arrays/Sort.java
@@ -0,0 +1,8 @@
+package practice.arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+
+        // Sort a list of integers
+    }
+}

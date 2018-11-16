75b191379842d15414b5bb9c1f3b7e07f7bde39a
gerlik <gerliks@gmail.com>
2018-11-16 10:44:37 +0200

    Bubble sort

--- src/practice/arrays/Sort.java
+++ src/practice/arrays/Sort.java
@@ -1,8 +1,42 @@
 package practice.arrays;
 
 public class Sort {
-    public static void main(String[] args) {
 
-        // Sort a list of integers
+    public static void main(String[] args) {
+        int[] myArray = {106, 26, 81, 5, 15};
+        int[] sorted = sortArray(myArray);
+        printArray(sorted);
+
     }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Sorted: " + array[i]);
+        }
+    }
+
+    public static int[] sortArray(int[] myArray) {
+        int[] sortedArray = new int[myArray.length];
+        for (int i = 0; i < myArray.length; i++) {
+            sortedArray[i] = myArray[i];
+        }
+        boolean flag = true;
+        int temp;
+
+        // Bubble sort
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+
 }
